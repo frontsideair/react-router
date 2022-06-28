38d41e0d914926213c3a2a5ac1303dbe6e4b2164
Matt Brophy <matt@brophy.org>
2022-06-28 10:35:36 -0400

    Update tests for aborted static query calls

--- packages/router/router.ts
+++ packages/router/router.ts
@@ -1778,7 +1778,8 @@ export function createStaticHandler(init: StaticHandlerInit): StaticHandler {
       );
 
       if (request.signal.aborted) {
-        throw new Error("query() aborted");
+        let method = isRouteRequest ? "queryRoute" : "query";
+        throw new Error(`${method}() call aborted`);
       }
     }
 
@@ -1860,12 +1861,14 @@ export function createStaticHandler(init: StaticHandlerInit): StaticHandler {
         callLoaderOrAction("loader", request, m, true, isRouteRequest)
       ),
     ]);
+
     if (request.signal.aborted) {
-      throw new Error("query() aborted");
+      let method = isRouteRequest ? "queryRoute" : "query";
+      throw new Error(`${method}() call aborted`);
     }
 
     // Can't do anything with these without the Remix side of things, so just
-    // cancel them for no
+    // cancel them for now
     results.forEach((result) => {
       if (isDeferredResult(result)) {
         result.deferredData.cancel();
